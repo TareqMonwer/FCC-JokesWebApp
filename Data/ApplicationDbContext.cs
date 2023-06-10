234bca0ecdd1e10733f1fdd507a759f1bf6aa0ce
Tarek Monwer <tarek.monwer@niftycoders.com>
2023-06-11 00:38:09 +0600

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FCF_JokesWebApp.Models;
+
+namespace FCF_JokesWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<FCF_JokesWebApp.Models.Joke>? Joke { get; set; }
+    }
+}
